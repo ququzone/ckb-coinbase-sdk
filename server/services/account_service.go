e58e4167fa4bda1b74b59848ef5f85dd40cee938
ququzone <xueping.yang@gmail.com>
2020-07-02 14:53:39 +0800

    change account balance blocknumber to indexer cell data blocknumber

--- server/services/account_service.go
+++ server/services/account_service.go
@@ -39,6 +39,11 @@ func (s *AccountAPIService) AccountBalance(
 		return nil, AddressError
 	}
 
+	header, err := s.client.GetTip(context.Background())
+	if err != nil {
+		return nil, RpcError
+	}
+
 	var total uint64
 	cells, err := s.client.GetCells(context.Background(), &indexer.SearchKey{
 		Script:     addr.Script,
@@ -63,9 +68,25 @@ func (s *AccountAPIService) AccountBalance(
 		}
 	}
 
-	header, err := s.client.GetTip(context.Background())
-	if err != nil {
-		return nil, RpcError
+	if total > 0 {
+		if cells.Objects[len(cells.Objects)-1].BlockNumber > header.BlockNumber {
+			header, err := s.client.GetHeaderByNumber(context.Background(), cells.Objects[len(cells.Objects)-1].BlockNumber)
+			if err != nil {
+				return nil, RpcError
+			}
+			return &types.AccountBalanceResponse{
+				BlockIdentifier: &types.BlockIdentifier{
+					Index: int64(header.Number),
+					Hash:  header.Hash.String(),
+				},
+				Balances: []*types.Amount{
+					{
+						Value:    fmt.Sprintf("%d", total),
+						Currency: CkbCurrency,
+					},
+				},
+			}, nil
+		}
 	}
 
 	return &types.AccountBalanceResponse{
